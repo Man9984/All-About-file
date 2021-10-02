fe22c1bb33cc2974dbe1215058a3abf6e1ec0fcd
Manjeet <manjeetyadav9984@gmail.com>
2021-10-02 20:35:45 +0530

    New package created

---
+++ man_package/m1.py
@@ -0,0 +1,6 @@
+
+# This program of addition of two numbers
+""" Created by Manjeet Kumar Yadav  """
+def add(a,b):
+    s=a+b
+    return s
---
+++ man_package/m2.py
@@ -0,0 +1,5 @@
+
+""" Program for Substracts Two Numbers ."""
+def sub(a,b):
+    s=a-b
+    return s
