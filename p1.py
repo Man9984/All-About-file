6f33b99e2a97189d2f1d8531d8376bd4f4c8ad9c
Manjeet <manjeetyadav9984@gmail.com>
2021-10-02 20:08:42 +0530

    here the power mehtod is added

--- p1.py
+++ p1.py
@@ -10,13 +10,20 @@ def mul(a,b):
 def div(a,b):
     return a/b
 
+"""here we are adding a method for power to calculate to power of whole number."""
+
+def pow(n):
+    return n**2
+
 #PL
 
 a = int(input("Enter the number a:"))
-b = int(input("Enter the number b:"))
+# b = int(input("Enter the number b:"))
 
-sum = add(a,b)
-sub = sub(a,b)
-mul = mul(a,b)
-div = div(a,b)
-print(sum,sub,mul,div)
+# sum = add(a,b)
+# sub = sub(a,b)
+# mul = mul(a,b)
+# div = div(a,b)
+# print(sum,sub,mul,div)
+
+print(pow(a))
